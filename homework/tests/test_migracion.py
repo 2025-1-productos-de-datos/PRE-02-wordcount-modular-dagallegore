7d68b74af00e5f27436f92360e2e1eefb70eeee0
dagallegore <dagallegore@unal.edu.co>
2025-05-03 11:50:02 -0500

    Segundo cambio que faltaba en el de migración

--- homework/tests/test_migracion.py
+++ homework/tests/test_migracion.py
@@ -15,7 +15,7 @@ def test_migracion():
         raise FileNotFoundError("Archivo results no existe.")
 
     results = {}
-    with open("data/output/results.tsv", "r", encoding="utf-8") as f:
+    with open("data/output/wordcount.tsv", "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
             key, value = line.strip().split("\t")
